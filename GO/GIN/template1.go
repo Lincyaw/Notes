0d8b9b667c4c7a06f10063a21bb7b9fad28e4e3c
lincyaw <814750204@qq.com>
2021-05-02 10:36:38 +0800

    更新

--- GO/GIN/template1.go
+++ GO/GIN/template1.go
@@ -5,30 +5,33 @@ import (
 	"html/template"
 	"net/http"
 )
+
 type User struct {
-	Name string
-	Age int
+	Name   string
+	Age    int
 	Gender string
 }
-func sayHello(w http.ResponseWriter, r *http.Request){
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("hello.html")
 	if err != nil {
 		fmt.Printf("Parse template failed, err: %v\n", err)
 		return
 	}
 	u1 := User{
-		Name: "猪头",
+		Name:   "猪头",
 		Gender: "女",
-		Age: 20,
+		Age:    20,
 	}
-	t.Execute(w,u1)
+	t.Execute(w, u1)
 }
 
-func main(){
-	http.HandleFunc("/",sayHello)
-	err:=http.ListenAndServe(":8080", nil)
+func main() {
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Printf("HTTP server start failed, err: %v\n", err)
 		return
 	}
 }
+
